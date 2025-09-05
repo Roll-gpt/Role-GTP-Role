ff4e5aee559512fff69ee5a484be1e54e76efa7b
eun seok Sung <kehs38@gmail.com>
2025-09-05 13:19:46 +0900

    Simplify to basic Express server

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') return res.sendStatus(200);
+  next();
+});
+
+app.post('/api/chat', (req, res) => {
+  res.json({ message: 'Simple API working!' });
+});
+
+app.listen(process.env.PORT || 3000);
